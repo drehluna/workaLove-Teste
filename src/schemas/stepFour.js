6c3f149a5446fdde10f5f4815bfa5fe85a5f4510
André Luna <drehluna@MacBook-Air-de-Andre.local>
2021-10-25 21:51:13 -0300

    Alteracoes chat

--- src/schemas/stepFour.js
+++ src/schemas/stepFour.js
@@ -1,5 +1,5 @@
 import * as yup from 'yup';
 
 export default yup.object().shape({
-    email: yup.string().email().required('Informe um e-mail valido'),
+    email: yup.string().email('Informe um e-mail valido').required('Informe um e-mail valido'),
 })
